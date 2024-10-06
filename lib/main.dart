39666022d3061fa793f514aed06d51de038b6950
쿨뽀 <howon5@naver.com>
2024-10-06 23:29:30 +0900

    Flexible은 하드코딩되는 값을 UI 비율에 기반하여 유연하게 만들어줘.
    Expended는 쿨한 위젯이야.
    
    Pomodoros의 디자인 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro_clone/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
         colorScheme: ColorScheme.fromSwatch(
           backgroundColor: const Color(0xFFE7626C),
         ),
@@ -27,9 +24,7 @@ class _MyAppState extends State<MyApp> {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const Scaffold(
-        body: Center(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
